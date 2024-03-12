27b7b8d724835d3dd1af05565b7634834a6bdc37
nishitaniyuki <nishitani@hey.com>
2024-03-12 11:03:41 +0900

    make Popover movable

--- src/app/popover-suspense/page.tsx
+++ src/app/popover-suspense/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { Suspense } from "react";
+import { Suspense, cloneElement, useState } from "react";
+import { useMove } from "react-aria";
 import {
   Button,
   Dialog,
   DialogTrigger,
+  Heading,
   OverlayArrow,
   Popover,
 } from "react-aria-components";
@@ -22,22 +24,55 @@ function Comment() {
   return <p>{text}</p>;
 }
 
-export default function PopoverAndSuspense() {
+function MovablePopoverTrigger({ label, heading, body }) {
+  const initialPosition = { x: 0, y: 0 } as const;
+  const [position, setPosition] = useState<{ x: number; y: number }>(
+    initialPosition
+  );
+
+  const { moveProps } = useMove({
+    onMove({ deltaX, deltaY }) {
+      setPosition(({ x, y }) => ({
+        x: x + deltaX,
+        y: y + deltaY,
+      }));
+    },
+  });
+
+  const handleOpenChange = () => setPosition(initialPosition);
+
   return (
-    <DialogTrigger>
-      <Button>Read Comment</Button>
-      <Popover>
+    <DialogTrigger onOpenChange={handleOpenChange}>
+      <Button>{label}</Button>
+      <Popover
+        style={{
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }}
+      >
         <OverlayArrow>
           <svg width={12} height={12} viewBox="0 0 12 12">
             <path d="M0 0 L6 6 L12 0" />
           </svg>
         </OverlayArrow>
         <Dialog>
-          <Suspense fallback="loading">
-            <Comment />
-          </Suspense>
+          {cloneElement(heading, moveProps)}
+          {body}
         </Dialog>
       </Popover>
     </DialogTrigger>
+  );
+}
+
+export default function PopoverAndSuspense() {
+  return (
+    <MovablePopoverTrigger
+      label="read comment"
+      heading={<Heading>drag me</Heading>}
+      body={
+        <Suspense fallback="loading">
+          <Comment />
+        </Suspense>
+      }
+    ></MovablePopoverTrigger>
   );
 }
